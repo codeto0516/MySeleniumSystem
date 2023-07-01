63977d6fc17167126fecf1cfa5a66427d6948479
codeto0516 <codetaisei@gmail.com>
2023-07-01 15:04:41 +0900

    作業前プッシュ

--- Chrome.py
+++ Chrome.py
@@ -15,89 +15,126 @@ import os
 from pprint import pprint
 import subprocess
 
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+
 class Chrome:
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, port=None, profile=None, headress=False, extension_path=None):
 
+        # 起動確認
+        self.is_active = self._judge_active()
+
+
+        print("### オプション ################################################")
         self.options = Options() #ブラウザ制御のオプション設定を読み込み
 
-        # leof -i:9222を実行して起動確認
-        if self._judge_active():
+        if port: self._port(port)
 
-            #################################################################################################################
-            # 起動している場合
-            #################################################################################################################
-            self.options.add_experimental_option("debuggerAddress", "127.0.0.1:9222") # ポートから起動済みのウィンドウを指定する
+        if profile: self._profile(profile) # プロフィールを指定
 
-        else:
-            #################################################################################################################
-            # 起動していない場合
-            #################################################################################################################
-            # ポートを指定してChromeを起動
-            self.options.add_argument('--emote-debugging-port=9222') # ポートを指定して起動する
-
-            ### プロフィールを指定
-            self.options.add_argument('--user-data-dir=/Users/ogawa/Library/Application Support/Google/Chrome')
-            self.options.add_argument('--profile-directory=eldoah')
-
-            ### 省メモリ設定
-            self.options.add_argument('--no-sandbox')                 # セキュリティ対策などのchromeに搭載してある保護機能をオフにする。
-            self.options.add_argument('--disable-dev-shm-usage')      # ディスクのメモリスペースを使う。
-            self.options.add_argument('--remote-debugging-port=9222') # リモートデバッグフラグを立てる。
-            self.options.add_argument('--start-maximized')            # 起動時にウィンドウを最大化する
-            self.options.add_argument('--disable-gpu')              # GPUハードウェアアクセラレーションを無効にする 「headlessモードで暫定的に必要なフラグ(そのうち不要になる)
-        
-
-            ### その他
+        if not self.is_active:
             self.options.add_experimental_option('detach', True) # 処理終了後ウィンドウを閉じないように
             self.options.add_experimental_option("excludeSwitches", ["enable-logging"])  # よくわからん長文をコンソールに表示させない
             
-            #######################################################
-            # kwrgsに値があったら
-            #######################################################
-            # ヘッドレスモードを有効にする
-            if "headless" in kwargs and kwargs["headless"] == True:
-                print("ヘッドレスモードで起動します")
-                self.options.add_argument("--headless")                 # ヘッドレスモード起動
-                self.options.add_experimental_option('detach', False)   # 処理終了後webdriverを破棄する
+        if headress: 
+            if self.is_active:
+                print("Chromeが起動しているためヘッドレスモードを有効にできません。再起動してください")
+            else:
+                self._headless() # ヘッドレスモードを有効にする
+                
+        if extension_path: self._extension(extension_path) # 拡張機能を読み込む
 
-            # もし**kwrgsにextensionがあったら拡張機能を読み込む
-            if "extension" in kwargs:
-                self.options.add_argument(f'load-extension={kwargs["extension"]}')
+        self._memory_saving() ### 省メモリ設定
 
 
 
+    ###########################################################################################################
+    ### オプション処理
+    ###########################################################################################################
+
+    def _port(self, port):
+        print(f"--- [ポート] {port}")
+        if self.is_active:
+            self.options.add_experimental_option("debuggerAddress", f"127.0.0.1:{port}") # ポートから起動済みのウィンドウを指定する
+        else:
+            self.options.add_argument(f'--remote-debugging-port={port}') # ポートを指定して起動する
+    
+    
+    def _profile(self, profile):
+        print(f"--- [プロフィール] {profile}")
+        user_data_dir = "./user-data"
+
+        # user-dataフォルダがなかったら作成
+        if not os.path.exists(user_data_dir): 
+            os.mkdir(user_data_dir)
+
+        # profileがなかったら
+        path = os.path.join(user_data_dir, profile)
+        if not os.path.exists(path): 
+            os.mkdir(path)
+
+
+        self.options.add_argument(f'--user-data-dir={user_data_dir}')
+        self.options.add_argument(f'--profile-directory={profile}')
+
+
+    def _headless(self):
+        print("--- [ヘッドレスモード] True")
+        self.options.add_argument("--headless")                 # ヘッドレスモード起動
+        self.options.add_experimental_option('detach', False)   # 処理終了後webdriverを破棄する
+
+
+    def _extension(self, extension_path):
+        print(f"--- [Chrome拡張機能] {extension_path}")
+        self.options.add_argument(f'load-extension={extension_path}')
+
+    def _memory_saving(self):
+        self.options.add_argument('--no-sandbox')                 # セキュリティ対策などのchromeに搭載してある保護機能をオフにする。
+        self.options.add_argument('--disable-dev-shm-usage')      # ディスクのメモリスペースを使う。
+        self.options.add_argument('--remote-debugging-port=9222') # リモートデバッグフラグを立てる。
+        self.options.add_argument('--start-maximized')            # 起動時にウィンドウを最大化する
+        self.options.add_argument('--disable-gpu')              # GPUハードウェアアクセラレーションを無効にする 「headlessモードで暫定的に必要なフラグ(そのうち不要になる)
+    
 
     ###########################################################################################################
     ### ブラウザ処理
     ###########################################################################################################
 
-    # Chromeが指定したポートで起動しているかどうか
+    # ポート9222で開いているメモリを取得する
+    @property
+    def lsof(self) -> list:
+        stdout:list = subprocess.run(['lsof', '-i:9222'], encoding='utf-8', stdout=subprocess.PIPE).stdout
+        return [item.split() for item in stdout.split("\n")[1:-1]]
+
+    # Chromeが指定したポートで起動しているかどうか （ポート9222で開いているメモリを取得する） 
     def _judge_active(self):
-        ### ポートで起動したかの確認（ポート9222で開いているウィンドウを取得する）
-        result = subprocess.run(['lsof', '-i:9222'], encoding='utf-8', stdout=subprocess.PIPE)
-        if result.returncode != 0 or not "Google" in str(result):
-            # 起動していない
-            print("ポートを指定してChromeを起動します")
-            return False
-        elif "Google" in str(result):
-            # 起動している
-            print("既に起動しているのでそのまま継続します")
+
+        # chromedriverを全部削除 Googleウィンドウだけ残す
+        [subprocess.run(["kill", item[1]]) for item in self.lsof if item[0] == "chromedri"]
+
+        # もう一度lsof -i:9222をしてGoogleウィンドウがあればそのまま使う
+        if self.lsof:
+            print("--- 既に起動しているのでそのまま継続します ---")
             return True
         else:
-            print("未知のエラー")
-            print(result)
-            exit()
-
+            print("--- ポートを指定してChromeを起動します ---")
+            return False
 
     # Chromeを起動
     def active(self):
-        print("Chromeを起動します")
-        ### ブラウザを起動 ###
-        self.driver = webdriver.Chrome(ChromeDriverManager().install(), options=self.options)
+        caps = DesiredCapabilities.CHROME
+        caps['goog:loggingPrefs'] = {'performance': 'ALL'}
+        print("--- Chromeを起動します ---")
+        self.driver = webdriver.Chrome(ChromeDriverManager().install(), options=self.options, desired_capabilities=caps)
         
 
     # Chromeを閉じる
-    def deactive(self):
-        print("Chromeを終了します")
+    def close(self):
+        print("--- Chromeを終了します ---")
         self.driver.close()
+        self.driver.quit()
+
+    def restart(self):
+        print("--- Chromemを再起動します ---")
+        
--- __init__.py
+++ __init__.py
@@ -4,7 +4,8 @@ from .Window import Window
 
 class MySeleniumSystem:
     def __init__(self, *args, **kwargs) -> None:
-        self.chrome = Chrome(args, kwargs)
+        self.chrome = Chrome(*args, **kwargs)
+
         self.chrome.active()
         self.driver = self.chrome.driver
 
